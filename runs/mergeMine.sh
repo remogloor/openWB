e6775738150471c9817f8e49e8ee3867c08e9468
Remo Gloor <remo.gloor@gmail.com>
2020-04-11 01:04:25 +0200

    added allowHttpsSecurity

--- runs/mergeMine.sh
+++ runs/mergeMine.sh
@@ -1,4 +1,13 @@
 #/bin/sh
+sudo git merge --no-commit mine/allowHttpsSecurity
+if [ $? -eq 0 ]
+then
+	sudo git merge --abort
+	sudo git merge mine/allowHttpsSecurity -m "merged allowHttpsSecurity"
+else
+	sudo git merge --abort
+fi
+
 sudo git merge --no-commit mine/settingsSecurity
 if [ $? -eq 0 ]
 then
