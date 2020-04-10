e91589d8a952a3cf3dcf8b2c4be50c043f415474
Remo Gloor <remo.gloor@gmail.com>
2020-04-11 00:59:33 +0200

    removed https as it has been merged, added settingsSecurity

--- runs/mergeMine.sh
+++ runs/mergeMine.sh
@@ -1,9 +1,9 @@
 #/bin/sh
-sudo git merge --no-commit mine/allowHttpsSecurity
+sudo git merge --no-commit mine/settingsSecurity
 if [ $? -eq 0 ]
 then
 	sudo git merge --abort
-	sudo git merge mine/allowHttpsSecurity -m "merged allowHttpsSecurity"
+	sudo git merge mine/settingsSecurity -m "merged settingsSecurity"
 else
 	sudo git merge --abort
 fi
