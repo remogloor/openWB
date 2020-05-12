0fa069654eb3614bc02eae308e0a3020e9a5e5d0
Remo Gloor <remo.gloor@gmail.com>
2020-05-12 19:46:00 +0200

    remove settingsSecurity

--- runs/mergeMine.sh
+++ runs/mergeMine.sh
@@ -8,11 +8,11 @@ else
 	sudo git merge --abort
 fi
 
-sudo git merge --no-commit mine/settingsSecurity
-if [ $? -eq 0 ]
-then
-	sudo git merge --abort
-	sudo git merge mine/settingsSecurity -m "merged settingsSecurity"
-else
-	sudo git merge --abort
-fi
+#sudo git merge --no-commit mine/settingsSecurity
+#if [ $? -eq 0 ]
+#then
+#	sudo git merge --abort
+#	sudo git merge mine/settingsSecurity -m "merged settingsSecurity"
+#else
+#	sudo git merge --abort
+#fi
