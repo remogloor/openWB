ea3f20656f1d797d13761b319ccfdcd6aa93e712
Remo Gloor <remo.gloor@gmail.com>
2020-09-12 13:49:20 +0200

    remote merge of preventConfigCodeInjection

--- runs/mergeMine.sh
+++ runs/mergeMine.sh
@@ -1,12 +1,4 @@
 #/bin/sh
-sudo git merge --no-commit mine/preventConfigCodeInjection
-if [ $? -eq 0 ]
-then
-	sudo git merge --abort
-	sudo git merge mine/preventConfigCodeInjection -m "merged preventConfigCodeInjection"
-else
-	sudo git merge --abort
-fi
 
 #sudo git merge --no-commit mine/settingsSecurity
 #if [ $? -eq 0 ]
